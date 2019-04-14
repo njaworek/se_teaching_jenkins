09993ba990c3ae2eccda71798c72c55a667dce03
njaworek <njaworek@noreply.github.com>
2019-04-14 12:06:03 +0200

    It is showing the lists of plugins

--- scripts/jenkins_get_version.py
+++ scripts/jenkins_get_version.py
@@ -2,9 +2,12 @@ import jenkins
 
 server = jenkins.Jenkins('http://localhost:8080',
                          username='admin',
-                         password='')  # place password here
+                         password='a4ef7fcb464f4694acdbcaa4ac97cc79')  # place password here
 
 
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+plugins = server.get_plugins_info()
+print plugins
